d4ca0f537b91212714cac5fbc9c1de9e5ad46cda
aliyeff-f <eliyevnurlan811@gmail.com>
2024-12-08 22:40:53 +0400

    app javascript two

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,21 @@
 
 'use strict';
 
-const  $sidebar = document.querySelector('[data-sidebar')
+import { addEventOnElements } from "./utils";
+
+
+
+
+
+
+const  $sidebar = document.querySelector('[data-sidebar');
+
+const  $sidebarTogglers = document.querySelectorAll('data-sidebar-toggler');
+
+const $overlay = document.querySelector('data-sidebar-overlay');
+
+
+addEventOnElements($sidebarTogglers,'click', function(){
+    $sidebar.classList.toggle('active');
+    $overlay.classList.toggle('active');
+});
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,16 @@
 /**
- * @copyright codewithsadee 2023
+ * @copyright Nurlan 2024
  */
 
 'use strict';
+
+
+
+const addEventOnElements = function ($element, eventType, callback) {
+    $elements.forEach($element => $element.addEventOnElements(eventType,callback));
+}
+
+
+export{
+    addEventOnElements
+}
