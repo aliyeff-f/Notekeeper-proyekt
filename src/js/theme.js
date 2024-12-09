fd85ada970931d3a210a1e0b3326fae3ebdbe6c5
aliyeff-f <eliyevnurlan811@gmail.com>
2024-12-09 22:46:38 +0400

    Lighe & dark theme one

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,5 +1,21 @@
 /**
- * @copyright codewithsadee 2023
+ * @copyright Nurlan 2024
  */
 
 'use strict';
+
+
+const  storedTheme = localStorage.getItem('theme');
+
+const systemThemeIsDark = window.matchMedia('prefers-color-scheme: dark').matches;
+
+const initialTheme = storedTheme ?? (systemThemeIsDark ? 'dark' : 'light');
+
+document.documentElement.setAttribute('data-theme',initialTheme);
+
+
+
+window.addEventListener('DOMContentLoaded',function (){
+    const  $themeBtn = this.document.querySelector('[data-theme-btn]');
+    if ($themeBtn) $themeBtn.addEventListener('click', toggleTheme);
+})
